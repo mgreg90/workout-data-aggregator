c56ff311c06758391ab1fa6c49147ea3a62d05e9
Mike Gregory <mgregory8219@gmail.com>
2021-02-15 15:50:46 -0500

    FN-4 Fix Env Var Reading

--- server/src/main/kotlin/com/workoutdataaggregator/server/EnvVars.kt
+++ server/src/main/kotlin/com/workoutdataaggregator/server/EnvVars.kt
@@ -1,20 +1,61 @@
 package com.workoutdataaggregator.server
 
 import io.github.cdimascio.dotenv.dotenv
+import org.slf4j.LoggerFactory
+import java.lang.NumberFormatException
 
 object EnvVars {
-    var port: Int = 8080
-    var strongAppBaseUrl: String = ""
-    var strongAppUsername: String = ""
-    var strongAppPassword: String = ""
-    var strongAppApplicationId: String = ""
+    var port : Int = 0
+    lateinit var strongAppBaseUrl : String
+    lateinit var strongAppUsername : String
+    lateinit var strongAppPassword : String
+    lateinit var strongAppApplicationId : String
 
     fun init() {
-        val dotEnvFile = dotenv()
-        port = dotEnvFile["PORT"].toInt()
-        strongAppBaseUrl = dotEnvFile["STRONG_APP_BASE_URL"]
-        strongAppUsername = dotEnvFile["STRONG_APP_USERNAME"]
-        strongAppPassword = dotEnvFile["STRONG_APP_PASSWORD"]
-        strongAppApplicationId = dotEnvFile["STRONG_APP_APPLICATION_ID"]
+        val envReader = EnvVarReader()
+        port = envReader.readInt("PORT")
+        strongAppBaseUrl = envReader.readStr("STRONG_APP_BASE_URL")
+        strongAppUsername = envReader.readStr("STRONG_APP_USERNAME")
+        strongAppPassword = envReader.readStr("STRONG_APP_PASSWORD")
+        strongAppApplicationId = envReader.readStr("STRONG_APP_APPLICATION_ID")
+
+        envReader.validate()
+    }
+}
+
+class EnvVarReader() {
+    private val missingEnvVars = mutableListOf<String>()
+    private val invalidEnvVars = mutableListOf<String>()
+    private val env = dotenv()
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun readInt(varName : String) = try {
+        read(varName)?.toInt() ?: 0
+    } catch (e : NumberFormatException) {
+        invalidEnvVars.add(varName)
+        0
+    }
+
+    fun readStr(varName : String) = read(varName) ?: ""
+
+    fun validate() {
+        when {
+            missingEnvVars.any() -> {
+                throw Exceptions.MissingEnvVarException(missingEnvVars)
+            }
+            invalidEnvVars.any() -> {
+                throw Exceptions.InvalidEnvVarException(invalidEnvVars)
+            }
+            else -> {
+                logger.info("Env Vars read successfully!")
+            }
+        }
+    }
+
+    private fun read(varName : String) : String? {
+        val value = env.get(varName)
+        if (value != null) return value
+        missingEnvVars.add(varName)
+        return null
     }
 }
--- server/src/main/kotlin/com/workoutdataaggregator/server/Exceptions.kt
+++ server/src/main/kotlin/com/workoutdataaggregator/server/Exceptions.kt
@@ -6,5 +6,19 @@ object Exceptions {
     enum class Types {
         LoginFailure
     }
-    class ClientException(override val message: String, val type: Types): Exception(message)
+    class ClientException(override val message : String, val type: Types): Exception(message)
+
+    class MissingEnvVarException(missingEnvVars : List<String>) : Exception(msg(missingEnvVars)) {
+        companion object {
+            fun msg(missingEnvVars : List<String>) =
+                "Missing Environment Variables: ${missingEnvVars.joinToString(", ")}"
+        }
+    }
+
+    class InvalidEnvVarException(missingEnvVars : List<String>) : Exception(msg(missingEnvVars)) {
+        companion object {
+            fun msg(invalidEnvVars : List<String>) =
+                "Unable to Parse Environment Variables: ${invalidEnvVars.joinToString(", ")}"
+        }
+    }
 }
